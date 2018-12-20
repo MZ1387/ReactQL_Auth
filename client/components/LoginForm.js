53cb9076231ac7d31b012ddcc32b2856a415256c
Mario Zamora <maza1387@gmail.com>
2018-12-19 21:34:21 -0500

    Add Error Handling to Login Mutation

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -5,10 +5,19 @@ import LoginMutation from '../mutations/Login';
 import AuthForm from './AuthForm';
 
 class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = { errors: [] };
+    }
+
     onSubmit({ email, password }) {
         this.props.mutate({
             variables: { email, password },
             refetchQueries: [{ query: CurrentUserQuery }]
+        }).catch((res) => {
+            const errors = res.graphQLErrors.map((err) => err.message);
+            this.setState({ errors });
         });
     }
 
@@ -16,7 +25,10 @@ class LoginForm extends Component {
         return (
             <div>
                 <h3>Login</h3>
-                <AuthForm onSubmit={this.onSubmit.bind(this)} />
+                <AuthForm 
+                    onSubmit={this.onSubmit.bind(this)} 
+                    errors={this.state.errors} 
+                />
             </div>
         );
     }
