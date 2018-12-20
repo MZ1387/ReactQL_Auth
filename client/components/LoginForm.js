1bfb407a688545321cd6258adec0899e55c9adca
Mario Zamora <maza1387@gmail.com>
2018-12-19 20:38:28 -0500

    Create AuthForm component

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -3,12 +3,14 @@ import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import CurrentUser from '../queries/CurrentUser';
 import Logout from '../mutations/Logout';
+import AuthForm from './AuthForm';
 
 class LoginForm extends Component {
     render() {
         return (
             <div>
-                Login
+                <h3>Login</h3>
+                <AuthForm />
             </div>
         );
     }
